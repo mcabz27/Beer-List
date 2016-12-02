712de3e858f98ea7f424b796b7a019654981f66b
Matthew Cabezon <matt.cabezon@gmail.com>
2016-12-01 20:18:42 -0500

    fix stuff

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log('it loaded!');
-  var myApi = '**';
+  var myApi = '8788a9d8ef81f87cc310c954b394aaa0';
 
 
 var brewery = function(search){
@@ -11,6 +11,12 @@ var brewery = function(search){
         // for(var i = 0; i < response.data.length; i++){
         console.log(response);
         console.log(response.data[0].name);
+        console.log(response.data[0].description);
+        console.log(response.data[0].images.medium);
+        name = response.data[0].name;
+        description = response.data[0].description;
+        pic = response.data[0].images.medium;
+        appendSearch(name, description, pic)
         }
       })
     };
@@ -26,12 +32,11 @@ var brewery = function(search){
   searchButton();
 
 
-  // var appendSearch = function(name, description, abv, food){
-  //   $('#allBeer').html('You searched ' + name);
-  //   $('#abv').html('The alcohol by volume is ' + abv + '%');
-  //   $('#description').html('Description: ' + description);
-  //   $('#foods').html('Food pairings: ' + food);
-  // };
+  var appendSearch = function(name, description, pic){
+    $('#allBeer').html('You searched ' + name);
+    $('#description').html(description);
+    $('#theimage').attr('src', pic);
+  };
 
 
   $('input.autocomplete').autocomplete({
